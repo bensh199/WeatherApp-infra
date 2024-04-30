542543d16ff176b09e295c9d49a5c68e0eb69c22
ben.shahar <benshahar99@gmail.com>
2024-04-30 16:07:58 +0300

    .

--- Terraform/Terraform-modules/Deployment/main.tf
+++ Terraform/Terraform-modules/Deployment/main.tf
@@ -1,37 +1,37 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "weatherapp-eks-state-backend"
-#     key            = "terraform.tfstate"
-#     region         = "il-central-1"
-#     dynamodb_table = "WeatherApp-Eks-state-backend"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "weatherapp-eks-state-backend"
+    key            = "terraform.tfstate"
+    region         = "il-central-1"
+    dynamodb_table = "WeatherApp-Eks-state-backend"
+  }
+}
 
-# # Set up the network module
-# module "network" {
-#   source                 = "../modules/network"
-#   vpc_cidr               = var.vpc_cidr
-#   aws_availability_zones = var.aws_availability_zones
-# }
+# Set up the network module
+module "network" {
+  source                 = "../modules/network"
+  vpc_cidr               = var.vpc_cidr
+  aws_availability_zones = var.aws_availability_zones
+}
 
-# # Set up the EKS module
-# module "eks" {
-#   source           = "../modules/eks"
-#   subnet_ids       = module.network.subnet_ids
-#   nodes_subnet_ids = module.network.nodes_subnet_ids
-#   depends_on       = [module.network]
-# }
+# Set up the EKS module
+module "eks" {
+  source           = "../modules/eks"
+  subnet_ids       = module.network.subnet_ids
+  nodes_subnet_ids = module.network.nodes_subnet_ids
+  depends_on       = [module.network]
+}
 
-# # Set up the IAM OIDC module
-# module "iam" {
-#   source              = "../modules/iam-oidc"
-#   eks_oidc_issuer_url = module.eks.eks_oidc_issuer_url
-#   depends_on          = [module.network]
-# }
+# Set up the IAM OIDC module
+module "iam" {
+  source              = "../modules/iam-oidc"
+  eks_oidc_issuer_url = module.eks.eks_oidc_issuer_url
+  depends_on          = [module.network]
+}
 
 resource "null_resource" "ArgoCD-Init-Script" {
   provisioner "local-exec" {
-    command = "sh ${var.ROOT_PATH}/WeatherApp-infra/ArgoCD/ArgoCD-Init.sh"
+    command = "sh ${var.ROOT_PATH}/WeatherApp-infra/ArgoCD/ArgoCD-Init.sh --account_ID=${data.aws_caller_identity.current.account_id} --region=${var.aws_region} --cluster_name=${module.eks.cluster_name}"
   }
   #  --account_ID=${data.aws_caller_identity.current.account_id} --region=${var.aws_region} --cluster_name=${module.eks.cluster_name}
   # command = "sh ${var.ROOT_PATH}/WeatherApp-infra/ArgoCD/ArgoCD-Init.sh --account_ID=${var.ACCOUNT_ID} --region=${var.aws_region} --cluster_name=${module.eks.cluster_name}"
