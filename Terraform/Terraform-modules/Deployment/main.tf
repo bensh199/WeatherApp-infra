d55f9d55e51bd6411be42861b1254f72d3315a95
ben.shahar <benshahar99@gmail.com>
2024-04-30 12:29:05 +0300

    .

--- Terraform/Terraform-modules/Deployment/main.tf
+++ Terraform/Terraform-modules/Deployment/main.tf
@@ -14,25 +14,25 @@ module "network" {
   aws_availability_zones = var.aws_availability_zones
 }
 
-# Set up the EKS module
-module "eks" {
-  source           = "../modules/eks"
-  subnet_ids       = module.network.subnet_ids
-  nodes_subnet_ids = module.network.nodes_subnet_ids
-  depends_on       = [module.network]
-}
+# # Set up the EKS module
+# module "eks" {
+#   source           = "../modules/eks"
+#   subnet_ids       = module.network.subnet_ids
+#   nodes_subnet_ids = module.network.nodes_subnet_ids
+#   depends_on       = [module.network]
+# }
 
-# Set up the IAM OIDC module
-module "iam" {
-  source              = "../modules/iam-oidc"
-  eks_oidc_issuer_url = module.eks.eks_oidc_issuer_url
-  depends_on          = [module.network]
-}
+# # Set up the IAM OIDC module
+# module "iam" {
+#   source              = "../modules/iam-oidc"
+#   eks_oidc_issuer_url = module.eks.eks_oidc_issuer_url
+#   depends_on          = [module.network]
+# }
 
-resource "null_resource" "ArgoCD-Init-Script" {
-  provisioner "local-exec" {
-    command = "sh $(ROOT_PATH)/WeatherApp-infra/ArgoCD/ArgoCD-Init-personal.sh"
-  }
-    # Execute the local-exec provisioner after all modules have been deployed
-  depends_on = [module.network, module.eks, module.iam]
-}
+# resource "null_resource" "ArgoCD-Init-Script" {
+#   provisioner "local-exec" {
+#     command = "sh $(ROOT_PATH)/WeatherApp-infra/ArgoCD/ArgoCD-Init-personal.sh"
+#   }
+#     # Execute the local-exec provisioner after all modules have been deployed
+#   depends_on = [module.network, module.eks, module.iam]
+# }
